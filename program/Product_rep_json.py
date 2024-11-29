c718792512ae378e129c55f51038b6cce368ac48
Kurkumich <113376198+Kurkumich@users.noreply.github.com>
2024-11-29 23:51:37 +0300

    Добавил метод read_all

--- program/Product_rep_json.py
+++ program/Product_rep_json.py
@@ -1,1 +1,22 @@
+# --*-- encoding: utf-8 --*--
+import json
+from typing import List
+from decimal import Decimal
+from Product import Product
 
+
+class ProductRepJSON(Product):
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def read_all(self) -> List[Product]:
+        try:
+            with open(self.file_path, "r", encoding="utf-8") as file:
+                data = json.load(file)
+            return [Product.create_from_json(json.dumps(item)) for item in data]
+        except FileNotFoundError:
+            print(f"Файл {self.file_path} не найден. Возвращается пустой список.")
+            return []
+        except json.JSONDecodeError as e:
+            print(f"Ошибка декодирования JSON: {e}")
+            return []
