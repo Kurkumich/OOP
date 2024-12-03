3298ca14a2b7a0e46f38d85e595924b10a0f67d6
Kurkumich <113376198+Kurkumich@users.noreply.github.com>
2024-12-03 23:30:29 +0300

    изменил файлы с учетом добавления стратегии

--- program/Product_rep_json.py
+++ program/Product_rep_json.py
@@ -1,28 +1,25 @@
-#--*-- encoding: utf-8 --*--
 import json
 from typing import List
-from Product import Product
-from decimal import Decimal
-from ProductRepository import ProductRepository
+from ProductRepFileStrategy import ProductRepFileStrategy
+import os
 
 
-class ProductRepJSON(ProductRepository):
-    def read_all(self) -> List[Product]:
+class JSONFileStrategy(ProductRepFileStrategy):
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def read(self) -> List[dict]:
+        if not os.path.exists(self.file_path):
+            return []
         try:
             with open(self.file_path, "r", encoding="utf-8") as file:
-                data = json.load(file)
-            return [Product.create_from_json(json.dumps(item)) for item in data]
-        except FileNotFoundError:
-            print(f"Файл {self.file_path} не найден. Возвращается пустой список.")
-            return []
+                return json.load(file)
         except json.JSONDecodeError as e:
             print(f"Ошибка декодирования JSON: {e}")
             return []
 
-    def write_all(self, products: List[Product]) -> None:
-        try:
-            data = [json.loads(product.to_json()) for product in products]
-            with open(self.file_path, "w", encoding="utf-8") as file:
-                json.dump(data, file, ensure_ascii=False, indent=4)
-        except Exception as e:
-            print(f"Ошибка записи в файл {self.file_path}: {e}")
+    def write(self, data: List[dict]) -> None:
+        with open(self.file_path, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+
